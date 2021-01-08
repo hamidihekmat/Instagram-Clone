6ca770e5c09c49fd9b92104f7b9cee259f72e3d9
Hekmat Hamidi <theonehamidi@gmail.com>
2021-01-07 20:10:56 -0500

    changed background color

--- pages/index.js
+++ pages/index.js
@@ -2,17 +2,18 @@ import { Box, Text } from '@chakra-ui/react';
 
 export default function Home() {
   return (
-    <Box
-      p={4}
-      w={{
-        sm: '100%',
-        lg: '60%',
-      }}
-      maxWidth="975px"
-      bg="primary.bg"
-      m="auto"
-    >
-      <Text>Hello World</Text>
+    <Box w="100%" bg="primary.bg">
+      <Box
+        p={4}
+        w={{
+          sm: '100%',
+          lg: '60%',
+        }}
+        maxWidth="975px"
+        m="auto"
+      >
+        <Text>Hello World</Text>
+      </Box>
     </Box>
   );
 }
